51780cd144d25d81ad6f915019102f72e4687f5d
Mariana Granados <mgranados09@alumnos.uaq.mx>
2023-06-15 20:57:51 -0600

    works now up to 60s, change output pin to a1 also

--- timer.c
+++ timer.c
@@ -7,11 +7,14 @@
 
 #BYTE PORTD=0xF83
 #BYTE TRISD = 0xF95
+#BYTE PORTB=0xF81
+#BYTE TRISB=0xF93
 
 int16 const display[]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 int1 count = 0;
-int value = 0;
+int time = 0;
 
+int16 dec, un;
 
 
 #int_TIMER1
@@ -20,12 +23,10 @@ void TIMER1_isr(void)
    
    if(count == 1)
    {
-      output_toggle(pin_b1);
-      value++;
-      if(value < 10 && value >= 0)
-      {
-         PORTD = display[value];
-      }
+      output_toggle(pin_a1);
+      time++;
+      dec = time / 10;
+      un = time % 10;  
    }
    set_timer1(3036); //recharge timer1
    count++;
@@ -35,11 +36,20 @@ void main(void)
 {  
    
    TRISD = 0x00;
+   TRISB = 0x00;
    setup_timer_1(T1_INTERNAL | T1_DIV_BY_4);
    set_timer1(3036);
    enable_interrupts(INT_TIMER1);
    enable_interrupts(global);
-   PORTD = display[value];
-   while(true);
+   PORTD = display[un];
+   while(true)
+   {
+      if(dec <= 6)
+      {
+          PORTD = display[un];
+          PORTB = display[dec];
+      }
+   
+   }
 
 }
